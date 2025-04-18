12874ea827b3cf1308d666a1f8345c58f6e38567
Teerapong <jagteerapong@gmail.com>
2025-04-18 14:08:41 +0700

    Test Git flow

--- src/main/java/com/account/constants/AppConstant.java
+++ src/main/java/com/account/constants/AppConstant.java
@@ -4,4 +4,8 @@ public class AppConstant {
 	public class SystemLogon {
 		public static final String ACTIVE_STATUS_Y = "Y";
 	}
+	
+	public class TestGitFlow {
+		public static final String TEST_GIT_FLOW = "TEST_GIT_FLOW";
+	}
 }
